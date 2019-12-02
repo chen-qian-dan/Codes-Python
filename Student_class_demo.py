e8bcd3b5868e4ee42cdf060c439eafffda4d2c5b
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-12-02 10:30:43 +1030

    Student class demo.

---
+++ Student_class_demo.py
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "IT", 4.5, False)
+
+print(student2.gpa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
