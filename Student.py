00b1e05ca58db3bc114df8ec342cc7b2db8fd114
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-12-02 20:54:06 +1030

    Student class.

--- Student.py
+++ Student.py
@@ -7,8 +7,11 @@ class Student:
         self.gpa = gpa
         self.is_on_probation = is_on_probation
 
-
-
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
 
 
 
