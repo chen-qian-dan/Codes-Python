715bcaecadcef625b7c80af5aa8e5d15eee6a6ec
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-01-08 21:53:04 +1030

    Recursion demo: Fibonacci.

---
+++ Recursion_Fibonacci.py
@@ -0,0 +1,23 @@
+
+
+# Function for nth Fibonacci number
+def Fibonacci(n):
+    """
+    F0 = 0
+    F1 = 1
+    Fn = Fn-1 + Fn-2
+    :param n:
+    :return:
+    """
+    if n < 0:
+        # first Fibonacci number is zero;
+        print("Incorrect input")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+print(Fibonacci(9))
+
