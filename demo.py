9984610f546d6eb0008075a7a916938d2bcb5ac8
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-01-28 23:58:46 +1030

    demo.

--- demo.py
+++ demo.py
@@ -1,22 +1,64 @@
-from classEmployee import Employee
 
-a = Employee("Qian", "100000")
-b = Employee("Chen", "120000")
+total = 1 + \
+        2 + \
+        3
 
-print(a.empCount)
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
 
-from classEmployee import Student
-
-c = Student("Qian", "1611729")
-d = Student("Chen", "0020223")
-print(c.studentCount)
+print(days)
 
 
 
 
 
+import datetime
 
 
+
+def braces(values):
+    '''
+    :param values: an array of strings
+    :return:
+    '''
+    no = "NO"
+    yes = "YES"
+    ret = []
+    for s in values:
+        while "()" in s or "[]" in s or "{}" in s:
+            if "()" in s:
+                s = s.replace("()", "")
+            elif "[]" in s:
+                s = s.replace("[]", "")
+            elif "{}" in s:
+                s = s.replace("{}", "")
+        if s:
+            ret.append(no)
+        else:
+            ret.append(yes)
+
+    return ret
+
+
+dateIn = input()
+year = int(dateIn[-4:])
+month = int(dateIn[3:5])
+day = dateIn[0:2]
+
+day = 1
+
+nextMeeting = datetime.date(year, month, day)
+
+print(nextMeeting)
+
+while nextMeeting.weekday() != 1:
+    nextMeeting += datetime.timedelta(days = 1)
+
+
+ret = "Tue-" + str(day) + "-" + str(month) + str(year) + ":("
+#print(ret)
+
+#print(nextMonth.weekday())
 
 
 
