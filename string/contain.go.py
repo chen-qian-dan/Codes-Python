412130f68dce1f606e63c0c38521a8b20567ae5c
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-01-30 15:22:45 +1030

    check if a sub string in a string.

---
+++ string/contain.go.py
@@ -0,0 +1,20 @@
+
+val = "Qian Chen"
+
+if "q" in val:
+    print("q in val")
+else:
+    print("q is not in val")
+
+
+if "Ch" in val:
+    print("Ch in val")
+else:
+    print("Ch is not in val")
+
+
+
+
+
+
+
