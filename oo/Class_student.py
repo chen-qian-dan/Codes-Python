0120faa08cf521563c7838a413cc6fe370d2a40c
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-02-20 22:59:20 +1030

    Class student

---
+++ oo/Class_student.py
@@ -0,0 +1,22 @@
+
+# define a class with constructor
+class Student:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+
+s1 = Student('Qian', 35, 'F')
+print(s1.name)
+
+
+
+
+
+
+
+
+
+
+
